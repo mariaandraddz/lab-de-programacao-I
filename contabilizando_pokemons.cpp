da2056352752bf6be645bcdce8645573fc2c1aa7
Maria Eduarda Andrade <160553521+mariaandraddz@users.noreply.github.com>
2026-09-09 23:20:54 -0300

    Add basic Pokemon counting program

---
+++ contabilizando_pokemons.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main() 
+{
+    int k, j, h;
+    int a, b, c;
+        cin>>k>>j>>h;
+        cin>>a>>b>>c;
+        int soma1;
+        cout<<k+a<< " " <<j+b<< " " <<h+c<< " " <<endl;
+        return 0;
+  
+        
+}
